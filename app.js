c2ab7df38014097221aa0d7345c14505fff5b1f4
ViDA_TG <victorademiju.t@gmail.com>
2023-01-02 13:28:20 +0100

    Connect to db

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
 
 const app = express();
 app.set('view engine', 'ejs');
 
+const dbURi = 'mongodb+srv://ViDA:test12345@cluster0.3hmjro0.mongodb.net?retryWrites=true&w=majority';
+mongoose.connect(dbURi)
+.then((result) => {console.log(app.listen(5000))})
+.catch((err) => {console.log("Error connecting")})
 
-app.listen(5000);
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     const blogs = [
-        {title: 'How I met your mother', snippet: 'B11', description: 'Bla'},
-        {title: 'How I met your father', snippet: 'B22', description: 'Blalablu'},
+        {title: 'How I met your mother', snippet: 'B11', description: "It's a dying earth fantasy story about found families. It kicks off amid the dunes with a birth in the family. (Note that: the old are not wise in this story). And there's a twist! The prophecy does not say that the protagonist will be the Chosen One."},
+        {title: 'How I met your father', snippet: 'B22', description: 'Jessica walked over to the window and reflected on her cold surroundings. She had always loved industrial Glasgow with its anxious, ashamed arches. It was a place that encouraged her tendency to feel fuzzy. Then she saw something in the distance, or rather someone. It was the a thoughtful figure of Mike Lakeman.'},
     ]
     res.render('index', { title: 'Home', blogs });
 })
@@ -28,5 +37,5 @@ app.get('/blogs/create', (req, res) => {
     res.render('createBlog', { title: 'Create'});
 })
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title:"404"});
 })
