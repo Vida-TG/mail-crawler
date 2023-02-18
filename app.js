58372a2216d48effab942b717f402e4e8f24d2b7
Vida-TG <victorademiju.t@gmail.com>
2023-02-18 15:50:57 +0100

    Index

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'How I met your mother', snippet: 'B11', description: "It's a dying earth fantasy story about found families. It kicks off amid the dunes with a birth in the family. (Note that: the old are not wise in this story). And there's a twist! The prophecy does not say that the protagonist will be the Chosen One."},
-        {title: 'How I met your father', snippet: 'B22', description: 'Jessica walked over to the window and reflected on her cold surroundings. She had always loved industrial Glasgow with its anxious, ashamed arches. It was a place that encouraged her tendency to feel fuzzy. Then she saw something in the distance, or rather someone. It was the a thoughtful figure of Mike Lakeman.'},
+        {title: 'How I met your sister', snippet: 'B22', description: 'Jessica walked over to the window and reflected on her cold surroundings. She had always loved industrial Glasgow with its anxious, ashamed arches. It was a place that encouraged her tendency to feel fuzzy. Then she saw something in the distance, or rather someone. It was the a thoughtful figure of Mike Lakeman l figure of Mike Lakeman.'},
     ]
     res.render('index', { title: 'Home', blogs });
 })
