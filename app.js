908c0b8114dce3b79eae1dbd3330eba414a9bacf
Vida-TG <victorademiju.t@gmail.com>
2023-03-04 03:00:47 +0100

    App

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    //res.send('<p>Main About Page</p>')
+    //res.send('<p>About Page Request</p>')
     //res.sendFile('./views/about.html', { root: __dirname });
     res.render('about', {title: 'About'});
 })
